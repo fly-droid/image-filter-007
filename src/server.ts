c400270ac677399f6a8dd35765f8c9861cd79dd1
EC2 Default User <ec2-user@ip-172-31-12-22.ec2.internal>
2022-08-30 08:52:19 +0000

    added a filter image function to the server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {Router,Request,Response} from 'express';
 
 (async () => {
 
@@ -28,6 +29,18 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get('./filteredimage', async(req: Request, res: Response) =>{
+    const image_url = req.query.image_url.toString(); 
+    if(!image_url){
+      res.status(400).send('image url is required');
+      
+    }
+    const filteredimage = await filterImageFromURL(image_url);
+     res.status(200).sendFile(filteredimage, () => { deleteLocalFiles([filteredimage]);
+     })
+       
+     });
+  
 
   //! END @TODO1
   
